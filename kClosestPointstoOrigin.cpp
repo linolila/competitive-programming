e12b1d2d8f83ba7c8bbe62eee0c1fc60187d9380
Jitu_Tesfaye <100919238+linolila@users.noreply.github.com>
2023-02-02 21:55:08 +0300

    Create kClosestPointstoOrigin.cpp

---
+++ kClosestPointstoOrigin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n=points.size();
+        vector<pair<int,pair<int,int>>>v;
+        int dist;
+        for(auto it:points){
+            dist=it[0]*it[0]+ it[1]*it[1];
+            v.push_back({dist,{it[0],it[1]}});
+        }
+        sort(v.begin(),v.end());
+        vector<vector<int>>ans(k);
+        for(int i=0;i<k;++i){
+            ans[i].push_back(v[i].second.first);
+            ans[i].push_back(v[i].second.second);
+        }
+        return ans;
+    }
+};
