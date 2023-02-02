fc54d315e4e69e5fe7988585f766b6a5eb4023b8
Jitu_Tesfaye <100919238+linolila@users.noreply.github.com>
2023-02-02 21:01:29 +0300

    Create  Target Indices After Sorting.cpp

---
+++ Target Indices After Sorting.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        int n =nums.size();
+        vector<int>result;
+        sort(nums.begin(),nums.end());
+        
+            for(int i=0;i<n;i++){
+                if(nums[i]==target)
+                {
+                    result.push_back(i);
+                }
+            }
+        return result;
+    }
+};
