aeedc308962b211fbce6e72d771ab497276c93bf
Jitu_Tesfaye <100919238+linolila@users.noreply.github.com>
2023-02-11 10:18:42 +0300

    Create Max Number of k-sum pairs.cpp

---
+++ Max Number of k-sum pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public: int maxOperations (vector <int>&nums, int k) {
+   sort(nums.begin(),nums.end());
+   int left=0,right=nums.size()-1;
+   int count=0;
+   while(left<right){
+       int sum=nums[left]+nums[right];
+       if(sum==k){
+           left++;
+           right--;count++;
+       }else if(sum>k){
+           right--;
+       }else
+           left++;
+       }
+        return count;
+   }
+  
+   };
