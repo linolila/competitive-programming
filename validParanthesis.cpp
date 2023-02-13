c91d31fd204035c112b8d8e10e7d7d7434ad6629
Jitu_Tesfaye <100919238+linolila@users.noreply.github.com>
2023-02-13 15:53:11 +0300

    Create validParanthesis.cpp

---
+++ validParanthesis.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool ismatching(char x,char y){
+        return ((x=='{' && y=='}')||(x=='[' && y==']')||(x=='(' && y==')'));
+    }
+public:
+    bool isValid(string s) {
+       stack<char>st;
+       for(int i=0;i<s.length();i++) 
+       {
+           if(s[i]=='{'|| s[i]=='['|| s[i]=='(')
+           st.push(s[i]);
+           else
+           {
+               if(st.empty())
+               return false;
+               if (ismatching(st.top(),s[i]))
+               st.pop();
+               else
+               return false;
+
+           }
+       }
+       if(st.empty())
+       return true;
+       else
+       return false;
+    }
+};
