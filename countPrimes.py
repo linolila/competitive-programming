049b8bc8ae97609a5fa41a7002b0b548e31c20b5
Jitu_Tesfaye <jitutesfaye17@gmail.com>
2023-12-18 22:19:00 +0300

    countprimwby python

---
+++ countPrimes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n == 0:
+            return 0
+        primes = [True for i in range(n+1)]
+        count = 0
+        p=2
+        while p * p <n:
+            if primes[p]:
+                for i in range(p*2,n+1,p):
+                    primes[i]=False
+            p+=1
+        primes[0],primes[1]= False, False
+        for p in range(n):
+               if primes[p]:
+                   count+=1
+        return count       
+                   
